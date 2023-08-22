c92dcec359ce59f49b0d07f33c1786691692b822
Thorley-Arya Öner <oner.thorley@gmail.com>
2023-08-21 20:35:02 -0500

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,6 +1,10 @@
 var app = new function() {
     this.name = "Masks", this.version = "1", this.date = "2009", this.folder = "asset-v1/", this.looptime = 7406, this.bpm = 130, this.totalframe = 354, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !2, this.bonusendloopA = !2, this.recmaxloop = 100, this.recminloop = 10, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#273C7F", this.col0 = "#273C7F", this.col1 = "#273C7F", this.col2 = "#273C7F", this.col3 = "#273C7F", this.col4 = "#273C7F", this.animearray = [
     {
+        name: "1_demon",
+        color: "#000000",
+        uniqsnd: !1
+    },{
         name: "16_hacker",
         color: "#000000",
         uniqsnd: !1
